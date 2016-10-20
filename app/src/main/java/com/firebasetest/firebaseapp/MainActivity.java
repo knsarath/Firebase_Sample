77c3558de19bb698e768873ec9d79a23aa4ff24a
sarath.kn <sarath.kn@robosoftin.com>
2016-10-20 18:00:26 +0530

    Installed and configured basic firebase setup

--- app/src/main/java/com/firebasetest/firebaseapp/MainActivity.java
+++ app/src/main/java/com/firebasetest/firebaseapp/MainActivity.java
@@ -1,8 +1,7 @@
 package com.firebasetest.firebaseapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
 
 import com.firebase.client.Firebase;
 
@@ -13,18 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Firebase.setAndroidContext(MainActivity.this);
         mRef = new Firebase(getString(R.string.firebase_db));
-
-        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Firebase mRefChild = new Firebase("Name");
-                mRefChild.setValue("Sarath");
-
-            }
-        });
-
     }
 }
