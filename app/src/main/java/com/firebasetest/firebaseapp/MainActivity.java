566b19c76a998f63a76cfc38aeaf4c3a61a4ef9a
sarath.kn <sarath.kn@robosoftin.com>
2016-10-21 12:51:40 +0530

    Added insertion of key , value

--- app/src/main/java/com/firebasetest/firebaseapp/MainActivity.java
+++ app/src/main/java/com/firebasetest/firebaseapp/MainActivity.java
@@ -12,7 +12,8 @@ public class MainActivity extends AppCompatActivity {
     private static final String NAME = "name";
     private Firebase mRootRef;
     private Button mButton;
-    private AppCompatEditText mEditText;
+    private AppCompatEditText mValue;
+    private AppCompatEditText mKey;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +23,16 @@ public class MainActivity extends AppCompatActivity {
         mRootRef = new Firebase(getString(R.string.firebase_db) + "Users"); // adding /Users at the end of base db url will add a new object called User
 
         mButton = (Button) findViewById(R.id.btn);
-        mEditText = (AppCompatEditText) findViewById(R.id.value);
+        mKey = (AppCompatEditText) findViewById(R.id.key);
+        mValue = (AppCompatEditText) findViewById(R.id.value);
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                final String value = mEditText.getText().toString();
-                //Firebase mRefChild = mRootRef.child(NAME);
+                final String key = mKey.getText().toString();
+                final String value = mValue.getText().toString();
 
-
-                mRootRef.push().setValue(value); // each time it creates a new id as the object property
+                Firebase childRef = mRootRef.child(key);
+                childRef.push().setValue(value); // each time it creates a new id as the object property
 
             }
         });
