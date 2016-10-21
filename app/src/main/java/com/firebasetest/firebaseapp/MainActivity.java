a81c9456516ab64c71cab1db60f6b76f48d03b76
sarath.kn <sarath.kn@robosoftin.com>
2016-10-21 16:04:52 +0530

    multiple data retrieval.  (key-value pair)

--- app/src/main/java/com/firebasetest/firebaseapp/MainActivity.java
+++ app/src/main/java/com/firebasetest/firebaseapp/MainActivity.java
@@ -2,12 +2,15 @@ package com.firebasetest.firebaseapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
 import android.widget.TextView;
 
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
+
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private Firebase mNameRef;
@@ -17,14 +20,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mNameRef = new Firebase(getString(R.string.firebase_db) + "Name"); // adding /Users at the end of base db url will add a new object called User
+        mNameRef = new Firebase(getString(R.string.firebase_db)); // adding /Users at the end of base db url will add a new object called User
         mTextView = (TextView) findViewById(R.id.name);
 
         mNameRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                final String value = dataSnapshot.getValue(String.class);
-                mTextView.setText(value);
+                final Map<String, String> map = dataSnapshot.getValue(Map.class);
+                final String name = map.get("name");
+                final String age = String.valueOf(map.get("Age"));
+                final String profession = map.get("Profession");
+                final String text = "Name : " + name + " <br> " + " Age : " + age + " <br> " + "  Profession :" + profession;
+                mTextView.setText(Html.fromHtml(text));
             }
 
             @Override
