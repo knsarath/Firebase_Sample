bb52c946c89f94b1bc3ab1fc8c362235faf5b361
sarath.kn <sarath.kn@robosoftin.com>
2016-10-21 15:41:57 +0530

    Basic string data retrieval.  (not key-value pair)

--- app/src/main/java/com/firebasetest/firebaseapp/MainActivity.java
+++ app/src/main/java/com/firebasetest/firebaseapp/MainActivity.java
@@ -1,38 +1,34 @@
 package com.firebasetest.firebaseapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.AppCompatEditText;
-import android.view.View;
-import android.widget.Button;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
+import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String NAME = "name";
-    private Firebase mRootRef;
-    private Button mButton;
-    private AppCompatEditText mValue;
-    private AppCompatEditText mKey;
+    private Firebase mNameRef;
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mNameRef = new Firebase(getString(R.string.firebase_db) + "Name"); // adding /Users at the end of base db url will add a new object called User
+        mTextView = (TextView) findViewById(R.id.name);
 
-        mRootRef = new Firebase(getString(R.string.firebase_db) + "Users"); // adding /Users at the end of base db url will add a new object called User
-
-        mButton = (Button) findViewById(R.id.btn);
-        mKey = (AppCompatEditText) findViewById(R.id.key);
-        mValue = (AppCompatEditText) findViewById(R.id.value);
-        mButton.setOnClickListener(new View.OnClickListener() {
+        mNameRef.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onClick(View view) {
-                final String key = mKey.getText().toString();
-                final String value = mValue.getText().toString();
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                final String value = dataSnapshot.getValue(String.class);
+                mTextView.setText(value);
+            }
 
-                Firebase childRef = mRootRef.child(key);
-                childRef.push().setValue(value); // each time it creates a new id as the object property
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
 
             }
         });
