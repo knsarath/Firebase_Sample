8d53565ea93c5ea44b8548ed32122ba75529b2b8
sarath.kn <sarath.kn@robosoftin.com>
2016-10-25 15:35:45 +0530

    list retrieval.

--- app/src/main/java/com/firebasetest/firebaseapp/MainActivity.java
+++ app/src/main/java/com/firebasetest/firebaseapp/MainActivity.java
@@ -2,36 +2,62 @@ package com.firebasetest.firebaseapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Html;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import com.firebase.client.ChildEventListener;
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
-import com.firebase.client.ValueEventListener;
 
-import java.util.Map;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
     private Firebase mNameRef;
-    private TextView mTextView;
+    private ListView mListView;
+    private ArrayList<String> mList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mNameRef = new Firebase(getString(R.string.firebase_db)); // adding /Users at the end of base db url will add a new object called User
-        mTextView = (TextView) findViewById(R.id.name);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mList);
+        mListView = (ListView) findViewById(R.id.list);
+        mListView.setAdapter(adapter);
 
-        mNameRef.addValueEventListener(new ValueEventListener() {
+        mNameRef.addChildEventListener(new ChildEventListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                final Map<String, String> map = dataSnapshot.getValue(Map.class);
-                final String name = map.get("name");
-                final String age = String.valueOf(map.get("Age"));
-                final String profession = map.get("Profession");
-                final String text = "Name : " + name + " <br> " + " Age : " + age + " <br> " + "  Profession :" + profession;
-                mTextView.setText(Html.fromHtml(text));
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                final String value = dataSnapshot.getValue(String.class);
+                mList.add(value);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                try {
+                    int index = Integer.parseInt(dataSnapshot.getKey());
+                    if (index < mList.size()) {
+                        mList.remove(index);
+                        mList.add(index, dataSnapshot.getValue(String.class));
+                        adapter.notifyDataSetChanged();
+
+                    }
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
             }
 
             @Override
